02e6ed8a0af0cdf0e10e6cfa3e29ace6c8acccee
omrom <omriip@gmail.com>
2022-08-21 16:50:01 +0300

    deploy heroklu

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+import bodyParser from "body-parser";
+import express from "express";
+import viewEngine from "./config/viewEngine"
+import initWebRoutes from "./routes/web"
+
+let app = express();
+
+//config view engine
+viewEngine(app);
+
+//config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({express: true}));
+
+//init web routes
+initWebRoutes(app);
+
+let port = process.env.port || 8080;
+
+app.listen(port, ()=> {
+    console.log("App is running at port : "+ port);
+});
