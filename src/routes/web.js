95f900ea135d3e61bc8045e8ece66e60be0c2b19
omrom <omriip@gmail.com>
2022-08-21 21:20:34 +0300

    updt

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,6 +6,7 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", chatbotController.test);
     router.get("/webhook", chatbotController.getWebhook);
+    router.get("/handm#webhook", chatbotController.getWebhook);
     router.post("/webhook", chatbotController.postWebhook);
     return app.use("/", router);
 };
