1ffdcfdfa9553f9679e7fe2f19b8a36dc62d87ae
omrom <omriip@gmail.com>
2022-09-04 16:23:25 +0300

    test

--- src/routes/web.js
+++ src/routes/web.js
@@ -11,23 +11,5 @@ let initWebRoutes = (app) => {
     return app.use("/", router);
 };
 
-// Verify that the callback came from Facebook.
-function verifyRequestSignature(req, res, buf) {
-    var signature = req.headers["x-hub-signature"];
-  
-    if (!signature) {
-      console.warn(`Couldn't find "x-hub-signature" in headers.`);
-    } else {
-      var elements = signature.split("=");
-      var signatureHash = elements[1];
-      var expectedHash = crypto
-        .createHmac("sha1", config.appSecret)
-        .update(buf)
-        .digest("hex");
-      if (signatureHash != expectedHash) {
-        throw new Error("Couldn't validate the request signature.");
-      }
-    }
-  }
 
 module.exports = initWebRoutes;
