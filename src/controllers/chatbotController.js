5fafb2cef6631dc3d6a84b4ab41aed1b91869ae7
omrom <omriip@gmail.com>
2022-10-19 18:47:18 +0300

    take data from monday

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -105,31 +105,21 @@ function handleMessage(sender_psid, received_message) {
         case 1:
           message = "מה נושא הפנייה?";
           response = {
-            "persistent_menu": [
-                {
-                    "locale": "default",
-                    "composer_input_disabled": false,
-                    "call_to_actions": [
-                        {
-                            "type": "postback",
-                            "title": "Talk to an agent",
-                            "payload": "CARE_HELP"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Outfit suggestions",
-                            "payload": "CURATION"
-                        },
-                        {
-                            "type": "web_url",
-                            "title": "Shop now",
-                            "url": "https://www.originalcoastclothing.com/",
-                            "webview_height_ratio": "full"
-                        }
-                    ]
-                }
+            "persistent_menu":[
+              {
+                "locale":"default",
+                "disabled_surfaces": ["CUSTOMER_CHAT_PLUGIN"],
+                "composer_input_disabled": false,      
+                "call_to_actions":[
+                  {
+                    "title":"My Account",
+                    "type":"postback",
+                    "payload":"PAYBILL_PAYLOAD"
+                  }
+                ]
+              }
             ]
-        }
+          }
           break;
         case 2:
           message = "על איזה קורס מדובר?";
